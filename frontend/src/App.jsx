481d91cd05840e549b191badaababf21302e8d0b
MashoodCk <mashoodck77@gmail.com>
2024-10-25 22:03:20 +0530

    project completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Box } from "@chakra-ui/react"
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/Navbar";
+
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Box>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  )
+}
+
+export default App;
